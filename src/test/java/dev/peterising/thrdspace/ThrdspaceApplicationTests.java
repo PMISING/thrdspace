722595dc6f5323ac8cb0cd445f66823d28d2a9f6
Peter Ising <pmising22@gmail.com>
2025-06-03 18:13:18 -0400

    Initial Spring Boot Project

---
+++ src/test/java/dev/peterising/thrdspace/ThrdspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.peterising.thrdspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThrdspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
